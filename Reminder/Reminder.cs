8559e9ca3d2d1cb8c1f0f4f76aca2af7e6c70d69
Seth Rider <seth.rider@gmail.com>
2018-04-12 22:28:07 +0100

    stuff and ting

--- Reminder/Reminder.cs
+++ Reminder/Reminder.cs
@@ -8,13 +8,21 @@ namespace Remember.Your.Id
     public static class Reminder
     {
         [FunctionName("Reminder")]
-        public static void Run([TimerTrigger("0 * * * * *")]TimerInfo myTimer, TraceWriter log)
+        public static void Run([TimerTrigger("0 */15 * * * *")]TimerInfo myTimer, TraceWriter log)
         {
             log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            if (DateTime.Now.DayOfWeek != DayOfWeek.Friday) return;
+            var tooEarly = new TimeSpan(6, 44, 0);
+            if (DateTime.Now.TimeOfDay < tooEarly) return;
+            var tooLate = new TimeSpan(9, 0, 0);
+            if (DateTime.Now.TimeOfDay > tooLate) return;
+
             const string message = "Remember your id...";
             var number = GetEnvironmentVariable("PhoneNumber");
             log.Info(number);
             Sms.Send(number, message, log);
+            Sms.Send(GetEnvironmentVariable("PhoneNumberSeth"), message, log);
         }
     }
 }
